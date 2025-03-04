128e4f7428c1cdf10f375d4d7cf8227444c28282
KuzminAleks <ofermadf@yandex.ru>
2025-03-04 12:44:31 +0300

    Исправить code style в ItemService

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto addItem(Integer userId, ItemDto item);
+
     ItemDto updateItem(Integer itemId, Integer userId, ItemDto item);
+
     ItemDto getItemById(Integer itemId);
+
     List<ItemDto> getItemOfOwner(Integer userId);
+
     List<ItemDto> getItemByText(String text);
 }
